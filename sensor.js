e0aeb269625188cd4159934d4394378c2fd4dfca
CowBelleh <brianlim_yisheng@hotmail.com>
2022-10-21 17:15:21 +0800

    Added new car on the road, damage and sensor applied to other car

--- sensor.js
+++ sensor.js
@@ -8,15 +8,15 @@ class Sensor {
     this.rays = [];
     this.readings = [];
   }
-  update(roadBorders) {
+  update(roadBorders, traffic) {
     this.#castRays();
     this.readings = [];
     for (let i = 0; i < this.rayCount; i++) {
-      this.readings.push(this.#getReading(this.rays[i], roadBorders));
+      this.readings.push(this.#getReading(this.rays[i], roadBorders, traffic));
     }
   }
 
-  #getReading(ray, roadBorders) {
+  #getReading(ray, roadBorders, traffic) {
     let touches = [];
     for (let i = 0; i < roadBorders.length; i++) {
       const touch = getIntersection(
@@ -29,6 +29,21 @@ class Sensor {
         touches.push(touch);
       }
     }
+    for (let i = 0; i < traffic.length; i++) {
+      const poly = traffic[i].polygon;
+      for (let j = 0; j < poly.length; j++) {
+        const touch = getIntersection(
+          ray[0],
+          ray[1],
+          poly[j],
+          poly[(j + 1) % poly.length]
+        );
+        if (touch) {
+          touches.push(touch);
+        }
+      }
+    }
+
     if (touches.length === 0) {
       return null;
     } else {
